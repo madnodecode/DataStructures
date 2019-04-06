483c046337a894190f9e97af5ecae3cb6caa1388
Madhan <madhan@JMS-Developer-9.local>
2019-04-06 15:04:22 -0700

    Trie-Insert

---
+++ src/TrieTries/Trie.java
@@ -0,0 +1,37 @@
+package TrieTries;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Trie {
+
+	private class TrieNode{
+		Map<Character,TrieNode> children;
+		boolean endOfWord;
+		
+		public TrieNode(){
+			children = new HashMap<>();
+			endOfWord = false;
+		}
+	}
+	private final TrieNode root;
+	public Trie(){
+		root = new TrieNode();
+	}
+	
+	public void insertInTrie(String word) {
+		TrieNode current = root;
+		for(int i=0;i<word.length();i++) {
+			char ch = word.charAt(i);
+			TrieNode node = current.children.get(ch);
+			if(node == null) {
+				node = new TrieNode();
+				current.children.put(ch, node);
+			}
+			current = node;
+		}
+		current.endOfWord = true;
+	}
+	
+	
+}
